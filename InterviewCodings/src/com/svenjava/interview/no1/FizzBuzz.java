b51825551c956f3b61099cb6c830d0044d47a198
Sven Schroeder <schuen@gmx>
2019-05-16 09:22:03 +0200

    finished first problem

--- InterviewCodings/src/com/svenjava/interview/no1/FizzBuzz.java
+++ InterviewCodings/src/com/svenjava/interview/no1/FizzBuzz.java
@@ -3,8 +3,17 @@ package com.svenjava.interview.no1;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		System.out.println("kurzer git test");
-
+		for(int i = 1; i <= 100; i++) {
+			if(i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
+		 	}else if(i % 5 == 0) {
+		 		System.out.println("Buzz");
+		 	}else if (i % 3 == 0) {
+		 		System.out.println("Fizz");
+		 	}else {
+		 		System.out.println(i);
+		 	}
+		}
 	}
 
 }

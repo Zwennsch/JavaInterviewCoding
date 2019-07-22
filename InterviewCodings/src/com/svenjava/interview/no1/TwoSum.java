a53e2cc158829ba95cd168ca3fc806863e712942
Sven Schroeder <schuen@gmx>
2019-07-22 22:15:12 +0200

    started on second task; TwoSum

---
+++ InterviewCodings/src/com/svenjava/interview/no1/TwoSum.java
@@ -0,0 +1,23 @@
+package com.svenjava.interview.no1;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {3,7,9,3,4,0};
+		
+		int value = 7;
+		
+		int[] indexes = new int[2];
+		
+		indexes = findSumFromArray(arr, value);
+
+	}
+
+	private static int[] findSumFromArray(int[] arr, int value) {
+		
+		
+		return null;
+	}
+
+}

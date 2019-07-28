56e44eda090048965542f2d573afcbb93bac2aa4
Sven Schroeder <schuen@gmx>
2019-07-28 11:12:33 +0200

    finished twoSum solution

--- InterviewCodings/src/com/svenjava/interview/no1/TwoSum.java
+++ InterviewCodings/src/com/svenjava/interview/no1/TwoSum.java
@@ -1,23 +1,72 @@
 package com.svenjava.interview.no1;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+	
+	static int[] res;
 
 	public static void main(String[] args) {
 		
-		int[] arr = {3,7,9,3,4,0};
+		int[] arr = {2,7,11,15};
 		
-		int value = 7;
+		int value = 9;
 		
-		int[] indexes = new int[2];
+		res  = new int[2];
 		
-		indexes = findSumFromArray(arr, value);
+		res = findSumFromArray(arr, value);
+		System.out.println(res[0]+ " "+ res[1]);
 
 	}
 
 	private static int[] findSumFromArray(int[] arr, int value) {
 		
+		Arrays.sort(arr);
+		int maxIndex = findMaximumPossibleIndex(arr, value);
+		System.out.println(maxIndex);
+		for(int i = maxIndex; i> 0; i--) {
+			for(int j = 0; j< maxIndex; j++) {
+				if(arr[i] + arr[j] == value) {
+					res[0] = arr[i];
+					res[1] = arr[j];
+					return res;
+				}else if(arr[i] + arr[j] > value) {
+					break;
+				}
+			}
+		}
+		return res;
 		
-		return null;
+		
+		
+		
+		
+	}
+//	professional solution; returns the indicies;
+	public static int[] getTwoSumProf(int[] numbers, int target) {
+		int[] res = new int[2];
+		Map<Integer, Integer> map = new HashMap<>();
+		for(int i = 0; i < numbers.length; i++) {
+			int delta = target - numbers[i];
+			if(map.containsKey(delta)) {
+				return new int[] {i, map.get(delta)};
+			}
+			map.put(numbers[i], i);
+		}
+		return res;
+	}
+
+	private static int findMaximumPossibleIndex(int[] arr, int value) {
+		int res = 0;
+		for (int i = arr.length-1; i > 0; i--) {
+			if(arr[i] <= value) {
+				res = i;
+				return res;
+			}
+		}
+		return 0;
 	}
 
 }

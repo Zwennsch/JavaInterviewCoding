528c72cabb14159221f26895819f75da17d20122
Sven Schroeder <schuen@gmx.de>
2019-07-29 08:55:19 +0200

    added new reverse String question

---
+++ InterviewCodings/src/com/svenjava/interview/no1/ReverseString.java
@@ -0,0 +1,15 @@
+package com.svenjava.interview.no1;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = "Test123";
+		test = "Tach12";
+		StringBuilder builder = new StringBuilder(test);
+		
+		System.out.println(builder.reverse());
+
+	}
+
+}
